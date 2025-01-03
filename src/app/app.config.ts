bb66e14f8fa27b843a558cf5f2c0811e9f316b19
Daniil Palii <daniil.palii@proton.me>
2025-01-03 19:03:16 +0100

    Configure palette, simplify PrimeNg configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { routes } from './app.routes';
-import provideTheme from './app.theme';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { definePreset } from '@primeng/themes';
+import auraPreset from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -11,6 +14,15 @@ export const appConfig: ApplicationConfig = {
 		provideRouter(routes),
 		provideClientHydration(withEventReplay()),
 		provideHttpClient(withFetch()),
-		provideTheme(),
+		provideAnimationsAsync(),
+		providePrimeNG({
+			theme: {
+				preset: definePreset(auraPreset),
+				options: {
+					cssLayer: { name: 'primeng' },
+					prefix: 'app',
+				},
+			},
+		})
 	],
 };
