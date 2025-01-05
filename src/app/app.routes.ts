88409c4849417c3b57161307780bf4445551179c
Daniil Palii <daniil.palii@proton.me>
2025-01-05 17:44:24 +0100

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TaskListComponent } from '../pages/task-list/task-list.component';
+import { TaskDetailsComponent } from '../pages/task-details/task-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: TaskListComponent },
+	{ path: 'tasks', component: TaskListComponent },
+	{ path: 'task-details', component: TaskDetailsComponent },
+];
