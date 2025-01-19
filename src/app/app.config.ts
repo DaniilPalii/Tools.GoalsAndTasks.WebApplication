bf445e339cbb6eb87121487bab15f9efabfa009a
Daniil Palii <daniil.palii@proton.me>
2025-01-19 18:14:55 +0100

    Use fetch in HttpClient for angular SSR optimization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { routes } from './app.routes';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { ThemeService } from './theming/theme.service';
@@ -14,8 +14,9 @@ export const appConfig: ApplicationConfig = {
 		provideRouter(routes),
 		provideClientHydration(withEventReplay()),
 		provideHttpClient(
+			withFetch(),
 			withInterceptors([
-				baseUrlInterceptor
+				baseUrlInterceptor,
 			])),
 		provideAnimationsAsync(),
 		providePrimeNG({
