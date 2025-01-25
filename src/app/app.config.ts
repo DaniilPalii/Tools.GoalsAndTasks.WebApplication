7092d6028d2b4f6d4f52d1418cbe0b619ba93770
Daniil Palii <daniil.palii@proton.me>
2025-01-25 21:54:21 +0100

    Improve fetching

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { routes } from './app.routes';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,7 +11,7 @@ import { baseUrlInterceptor } from '../transfer/base-url.interceptor';
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(routes),
+		provideRouter(routes, withComponentInputBinding()),
 		provideClientHydration(withEventReplay()),
 		provideHttpClient(
 			withFetch(),
--- src/pages/task-list/task-list.component.ts
+++ src/pages/task-list/task-list.component.ts
@@ -4,20 +4,21 @@ import { TableModule } from 'primeng/table';
 import { Card } from 'primeng/card';
 import { TaskDetails } from 'transfer/task-details';
 import { TaskService } from 'transfer/task.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
 	selector: 'app-task-list',
-	imports: [ ButtonModule, TableModule, Card ],
+	imports: [ ButtonModule, TableModule, Card, AsyncPipe ],
 	templateUrl: './task-list.component.html',
 	styleUrl: './task-list.component.css',
 })
 export class TaskListComponent implements OnInit {
-	public tasks: TaskDetails[] = [];
+	public tasks$!: Observable<TaskDetails[]>;
+
 	private readonly taskService = inject(TaskService);
 
 	public ngOnInit(): void {
-		this.taskService.getTasks().subscribe((tasks: TaskDetails[]) => {
-			this.tasks = tasks;
-		});
+		this.tasks$ = this.taskService.getTasks();
 	}
 }
