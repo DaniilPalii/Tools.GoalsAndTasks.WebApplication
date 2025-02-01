3bb596470b5ee71c086952ad7694cdc7672663b3
Daniil Palii <daniil.palii@proton.me>
2025-02-01 20:55:45 +0100

    Add tasks navigation in grid

--- src/pages/task-list/task-list.component.ts
+++ src/pages/task-list/task-list.component.ts
@@ -6,6 +6,7 @@ import { TaskDetails } from 'transfer/task-details';
 import { TaskService } from 'transfer/task.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-task-list',
@@ -17,8 +18,13 @@ export class TaskListComponent implements OnInit {
 	public tasks$!: Observable<TaskDetails[]>;
 
 	private readonly taskService = inject(TaskService);
+	private readonly router = inject(Router);
 
 	public ngOnInit(): void {
 		this.tasks$ = this.taskService.getTasks();
+	}
+
+	public navigateTo(taskId: number): void {
+		this.router.navigate([ 'tasks/1' ]);
 	}
 }
