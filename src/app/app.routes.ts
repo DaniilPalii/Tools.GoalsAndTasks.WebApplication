88e92ff0355aee81f364b8893ed850a999b7c6ac
Daniil Palii <daniil.palii@proton.me>
2025-02-08 15:13:46 +0100

    Add goal list page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { TaskListComponent } from 'pages/task-list/task-list.component';
 import { TaskDetailsComponent } from 'pages/task-details/task-details.component';
+import { GoalListComponent } from 'pages/goal-list/goal-list.component';
 
 export const routes: Routes = [
 	{ path: 'tasks', component: TaskListComponent },
 	{ path: 'tasks/:id', component: TaskDetailsComponent },
+	{ path: 'goals', component: GoalListComponent },
 	{ path: '', redirectTo: '/tasks', pathMatch: 'full' },
 ];
