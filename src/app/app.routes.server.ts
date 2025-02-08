a8c5b4e87407915ea0815982640ba335f610f2b8
Daniil Palii <daniil.palii@proton.me>
2025-02-08 15:12:45 +0100

    Make all routes rendered on client side

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,5 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-	{ path: 'tasks', renderMode: RenderMode.Prerender },
 	{ path: '**', renderMode: RenderMode.Client },
 ];
